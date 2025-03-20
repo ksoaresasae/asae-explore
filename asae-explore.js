bebef982261ae0a01218dd9555ac011d6bf43f27
ksoaresasae <127353108+ksoaresasae@users.noreply.github.com>
2025-03-20 17:42:44 -0400

    v7.45 one more time, with feelin'

--- asae-explore.js
+++ asae-explore.js
@@ -8,7 +8,7 @@ Developer:  Keith M. Soares - https://keithmsoares.com
 
 Version: 
 ------------
-7.44         2025-03-20
+7.45         2025-03-20
 Notes:
 - Added tabindex helper function
 - Fix Search and Chatbot alert messages 
@@ -23,7 +23,7 @@ Notes:
 
 //////////////////////////////////////////////
 // MASTER GITVERSION
-var gitVersion = "v7.44";
+var gitVersion = "v7.45";
 
 // MASTER BASE URL
 var thisBaseURL = "https://cdn.jsdelivr.net/gh/ksoaresasae/asae-explore@" + gitVersion + "/";
