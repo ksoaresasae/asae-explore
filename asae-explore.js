bd396e2b214d1d47b9593a0820ca3ac2e386f42d
ksoaresasae <127353108+ksoaresasae@users.noreply.github.com>
2025-04-03 13:31:25 -0400

    v7.57 update pb

--- asae-explore.js
+++ asae-explore.js
@@ -8,7 +8,7 @@ Developer:  Keith M. Soares - https://keithmsoares.com
 
 Version: 
 ------------
-7.56         2025-04-03
+7.57         2025-04-03
 Notes:
 - Added in-line reg buttons for events
 - Turn on Chatbot but keep Search off until AddSearch mod happens
@@ -26,7 +26,7 @@ Notes:
 
 //////////////////////////////////////////////
 // MASTER GITVERSION
-var gitVersion = "v7.56";
+var gitVersion = "v7.57";
 
 // MASTER BASE URL
 var thisBaseURL = "https://cdn.jsdelivr.net/gh/ksoaresasae/asae-explore@" + gitVersion + "/";
