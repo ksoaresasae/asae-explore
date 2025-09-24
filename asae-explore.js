67deb8fec69e7951546de0cfecb102fcc955aa82
ksoaresasae <127353108+ksoaresasae@users.noreply.github.com>
2025-09-23 20:44:02 -0400

    v7.81 white alert text actually

--- asae-explore.js
+++ asae-explore.js
@@ -8,7 +8,7 @@ Developer:  Keith M. Soares - https://keithmsoares.com
 
 Version: 
 ------------
-7.80         2025-09-23
+7.81         2025-09-23
 Notes:
 - Added announcement for Sept 29-Oct 1, 2025, infrastructure launch (SSO+Portal+Store)
 - Update AM to 2026 Indy and remove reg button
@@ -21,7 +21,7 @@ Notes:
 
 //////////////////////////////////////////////
 // MASTER GITVERSION
-var gitVersion = "v7.80";
+var gitVersion = "v7.81";
 
 // MASTER BASE URL
 var thisBaseURL = "https://cdn.jsdelivr.net/gh/ksoaresasae/asae-explore@" + gitVersion + "/";
